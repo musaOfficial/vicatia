a27edc3ff68a4ab1d7d058c8b38f3188768182c8
Musa Arslan <officialmusa58@gmail.com>
2023-11-24 15:29:03 +0100

    Project start 1.0

---
+++ components/pages/Homepage.js
@@ -0,0 +1,10 @@
+import classes from './Homepage.module.css';
+import Image from 'next/image';
+import BackgroundImage from './../../public/background-images/homebackground.png'
+function Homepage(){
+    return (
+        <div className={classes.container}>
+          
+        </div>
+    )
+} export default Homepage;
---
+++ components/pages/Signup.js
@@ -0,0 +1,9 @@
+import classes from './Signup.module.css'
+
+function Signup(){
+    return (
+        <div className={classes.container}>
+
+        </div>
+    )
+} export default Signup;
